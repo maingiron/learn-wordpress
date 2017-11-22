a95b1a6d602e2b1610e30ab5a5d5c51ca6a7eed2
Raphael Giron <maingiron@gmail.com>
2017-11-22 20:14:06 -0200

    Add main

--- index.php
+++ index.php
@@ -17,6 +17,18 @@
 			<div class="menu"></div>
 		</div>
 	</header>
+
+	<div class="conteudo">
+		<main>
+			<section class="slide"></section>
+			<section class="servicos"></section>
+			<section class="meio">
+				<aside class="barra-lateral"></aside>
+				<div class="noticias"></div>
+			</section>
+			<section class="mapa"></section>
+		</main>
+	</div>
 	
 </body>
 </html>

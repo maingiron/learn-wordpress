9a50acd98b226e9d1c33ba6619f02b8ad53809be
Raphael Giron <maingiron@gmail.com>
2017-11-22 20:38:45 -0200

    Add action to function carrega_script

--- functions.php
+++ functions.php
@@ -2,5 +2,7 @@
 
 	function carrega_scripts() {
 
-		wp_enqueue_styles('template', get_template_directory_uri() . '/assets/css/template.css');
+		wp_enqueue_styles('template', get_template_directory_uri() . '/assets/css/template.css', array(), '1.0', 'all');
 	}
+
+	add_action('wp_enqueue_styles', 'carrega_scripts');

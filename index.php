2f9c7ba104f092cf19ab406822fc3b2683fc8a4c
Raphael Giron <maingiron@gmail.com>
2017-11-22 20:11:40 -0200

    Add header

--- index.php
+++ index.php
@@ -5,6 +5,18 @@
 	<title>Learn WordPress</title>
 </head>
 <body <?php body_class(); ?>>
+
+	<header>
+		<div class="barra-topo">
+			<div class="redes-socias"></div>
+			<div class="pesquisa"></div>
+		</div>
+
+		<div class="area-menu">
+			<div class="logo"></div>
+			<div class="menu"></div>
+		</div>
+	</header>
 	
 </body>
 </html>

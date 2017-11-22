ff452e26d840cede73578ab84d0b33e9eff26924
Raphael Giron <maingiron@gmail.com>
2017-11-22 21:26:25 -0200

    Add wp_enqueue_script

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 	function carrega_scripts() {
 
 		wp_enqueue_style('template', get_template_directory_uri() . '/assets/css/template.css', array(), '1.0', 'all');
+		wp_enqueue_script('template', get_template_directory_uri() . '/assets/js/template.js', array(), '1.0', true);
 	}
 
 	add_action('wp_enqueue_scripts', 'carrega_scripts');

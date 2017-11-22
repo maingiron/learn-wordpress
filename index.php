9851a75691c9880b449984ebc34c5020a0d34ee3
Raphael Giron <maingiron@gmail.com>
2017-11-22 20:17:36 -0200

    Add footer.php

--- index.php
+++ index.php
@@ -1,38 +1,11 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-	<meta charset="utf-8">
-	<title>Learn WordPress</title>
-</head>
-<body <?php body_class(); ?>>
-
-	<header>
-		<div class="barra-topo">
-			<div class="redes-socias"></div>
-			<div class="pesquisa"></div>
-		</div>
-
-		<div class="area-menu">
-			<div class="logo"></div>
-			<div class="menu"></div>
-		</div>
-	</header>
-
-	<div class="conteudo">
-		<main>
-			<section class="slide"></section>
-			<section class="servicos"></section>
-			<section class="meio">
-				<aside class="barra-lateral"></aside>
-				<div class="noticias"></div>
-			</section>
-			<section class="mapa"></section>
-		</main>
-	</div>
-
-	<footer>
-		
-	</footer>
-	
-</body>
-</html>
+<div class="conteudo">
+	<main>
+		<section class="slide"></section>
+		<section class="servicos"></section>
+		<section class="meio">
+			<aside class="barra-lateral"></aside>
+			<div class="noticias"></div>
+		</section>
+		<section class="mapa"></section>
+	</main>
+</div>

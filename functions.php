bdb42d3c093b7f09167b421bdd2f6a907e055800
Raphael Giron <maingiron@gmail.com>
2017-11-22 20:53:48 -0200

    Corrigido chamada da functions carrega_script

--- functions.php
+++ functions.php
@@ -5,4 +5,4 @@
 		wp_enqueue_style('template', get_template_directory_uri() . '/assets/css/template.css', array(), '1.0', 'all');
 	}
 
-	add_action('wp_enqueue_styles', 'carrega_scripts');
+	add_action('wp_enqueue_scripts', 'carrega_scripts');

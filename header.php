80b20dc5e4dbad2518ca44f7ecf882b93cdefae3
Raphael Giron <maingiron@gmail.com>
2017-11-22 20:17:04 -0200

    Add header.php

--- header.php
+++ header.php
@@ -17,22 +17,3 @@
 			<div class="menu"></div>
 		</div>
 	</header>
-
-	<div class="conteudo">
-		<main>
-			<section class="slide"></section>
-			<section class="servicos"></section>
-			<section class="meio">
-				<aside class="barra-lateral"></aside>
-				<div class="noticias"></div>
-			</section>
-			<section class="mapa"></section>
-		</main>
-	</div>
-
-	<footer>
-		
-	</footer>
-	
-</body>
-</html>

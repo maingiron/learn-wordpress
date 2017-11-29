ff5cfc9e04f3ce86a9977a17876d39e92c43261b
Raphael Giron <maingiron@gmail.com>
2017-11-29 20:12:19 -0200

    Add loop post noticias

--- index.php
+++ index.php
@@ -3,28 +3,30 @@
 	<main>
 		<section class="slide">Slide</section>
 		<section class="servicos">Serviços</section>
-		<section class="meio">
-			<aside class="barra-lateral">Barra Lateral</aside>
-			<div class="noticias">
-				<?php 
-					// Se houver algum post
-					if(have_posts()) : 
-						// Enquanto houver algum post, chame o post...
-						while (have_posts()) : the_post();
-				?>
-					<h1><?php the_title(); ?></h1>
-					<p>Publicado em <?php echo get_the_date(); ?> por <?php the_author(); ?></p>
-					<p>Categorias: <?php the_category(' '); ?></p>
-					<p><?php the_tags('Tags: ', ', ') ?></p>
-					<p><?php the_content(); ?></p>
-				<?php 
-						endwhile;
-					else:
-				?>
-					<p>Não existe post...</p>
-				<?php 
-					endif;
-				?>
+		<section class="meio container">
+			<div class="row">
+				<aside class="barra-lateral col-md-3">Barra Lateral</aside>
+				<div class="noticias col-md-9">
+					<?php 
+						// Se houver algum post
+						if(have_posts()) : 
+							// Enquanto houver algum post, chame o post...
+							while (have_posts()) : the_post();
+					?>
+						<h1><?php the_title(); ?></h1>
+						<p>Publicado em <?php echo get_the_date(); ?> por <?php the_author(); ?></p>
+						<p>Categorias: <?php the_category(' '); ?></p>
+						<p><?php the_tags('Tags: ', ', ') ?></p>
+						<p><?php the_content(); ?></p>
+					<?php 
+							endwhile;
+						else:
+					?>
+						<p>Não existe post...</p>
+					<?php 
+						endif;
+					?>
+				</div>
 			</div>
 		</section>
 		<section class="mapa">Mapa</section>

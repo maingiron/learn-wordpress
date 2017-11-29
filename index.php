c2fa4c936375fbc64af6390b31f1457d74e7cb0f
Raphael Giron <maingiron@gmail.com>
2017-11-29 20:05:49 -0200

    Add loop post noticias

--- index.php
+++ index.php
@@ -5,7 +5,27 @@
 		<section class="servicos">Serviços</section>
 		<section class="meio">
 			<aside class="barra-lateral">Barra Lateral</aside>
-			<div class="noticias">Noticias</div>
+			<div class="noticias">
+				<?php 
+					// Se houver algum post
+					if(have_posts()) : 
+						// Enquanto houver algum post, chame o post...
+						while (have_posts()) : the_post();
+				?>
+					<h1><?php the_title(); ?></h1>
+					<p>Publicado em <?php echo get_the_date(); ?> por <?php the_author(); ?></p>
+					<p>Categorias: <?php the_category(' '); ?></p>
+					<p><?php the_tags('Tags: ', ', ') ?></p>
+					<p><?php the_content(); ?></p>
+				<?php 
+						endwhile;
+					else:
+				?>
+					<p>Não existe post...</p>
+				<?php 
+					endif;
+				?>
+			</div>
 		</section>
 		<section class="mapa">Mapa</section>
 	</main>

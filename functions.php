bc2c62b7358d86d6889cf85c2b9cbd504bea1c7d
Raphael Giron <maingiron@gmail.com>
2017-11-29 19:33:48 -0200

    Adicionado docblock

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php 
 
+//-----------------------------------------------------------------------------
+
+	/**
+	 * Função para carregamento dos scripts.
+	 */
 	function carrega_scripts() {
 
 		wp_enqueue_style('template', get_template_directory_uri() . '/assets/css/template.css', array(), '1.0', 'all');
@@ -11,3 +16,5 @@
 	}
 
 	add_action('wp_enqueue_scripts', 'carrega_scripts');
+
+//-----------------------------------------------------------------------------

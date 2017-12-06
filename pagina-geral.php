49eff34f73984e946c10be15ce991570f56a7c58
Raphael Giron <maingiron@gmail.com>
2017-12-06 21:11:18 -0200

    Add img

--- pagina-geral.php
+++ pagina-geral.php
@@ -3,6 +3,7 @@
  * Template Name: Páginas Gerais
  */
 get_header(); ?>
+<img class="img-responsive" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
 <div class="conteudo-wrapper">
 	<main>
 		<div class="conteudo container">

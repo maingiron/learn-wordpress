f150ebc96f2db8fb24f778f0b8c6ea91417d2698
Raphael Giron <maingiron@gmail.com>
2017-12-06 20:45:04 -0200

    Add paragrafo page home

--- page-home.php
+++ page-home.php
@@ -7,25 +7,7 @@
 			<div class="row">
 				<aside class="barra-lateral col-md-3">Barra Lateral</aside>
 				<div class="noticias col-md-9">
-					<?php 
-						// Se houver algum post
-						if(have_posts()) : 
-							// Enquanto houver algum post, chame o post...
-							while (have_posts()) : the_post();
-					?>
-						<h1><?php the_title(); ?></h1>
-						<p>Publicado em <?php echo get_the_date(); ?> por <?php the_author(); ?></p>
-						<p>Categorias: <?php the_category(' '); ?></p>
-						<p><?php the_tags('Tags: ', ', ') ?></p>
-						<p><?php the_content(); ?></p>
-					<?php 
-							endwhile;
-						else:
-					?>
-						<p>Não existe post...</p>
-					<?php 
-						endif;
-					?>
+					<p>Página de Home</p>
 				</div>
 			</div>
 		</section>

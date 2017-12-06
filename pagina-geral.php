6d1ff7c61f64a7c84d4bd3c0b3c6302cfe69560c
Raphael Giron <maingiron@gmail.com>
2017-12-06 20:55:05 -0200

    Add loop pagina geral

--- pagina-geral.php
+++ pagina-geral.php
@@ -3,13 +3,29 @@
  * Template Name: Páginas Gerais
  */
 get_header(); ?>
-<div class="conteudo">
+<div class="conteudo-wrapper">
 	<main>
-		<section class="meio container">
-			<div class="noticias col-md-9">
-				<p>Estrutura de páginal geral</p>
-			</div>
-		</section>
+		<div class="conteudo">
+			<?php 
+				// Se houver algum post
+				if(have_posts()) : 
+					// Enquanto houver algum post, chame o post...
+					while (have_posts()) : the_post();
+			?>
+				<h1><?php the_title(); ?></h1>
+				<p>Publicado em <?php echo get_the_date(); ?> por <?php the_author(); ?></p>
+				<p>Categorias: <?php the_category(' '); ?></p>
+				<p><?php the_tags('Tags: ', ', ') ?></p>
+				<p><?php the_content(); ?></p>
+			<?php 
+					endwhile;
+				else:
+			?>
+				<p>Não existe post...</p>
+			<?php 
+				endif;
+			?>
+		</div>
 	</main>
 </div>
 <?php get_footer(); ?>

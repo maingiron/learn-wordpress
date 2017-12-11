f939b58085e2abad0858385107d483b54e7d33be
Raphael Giron <maingiron@gmail.com>
2017-12-11 20:43:45 -0200

    add class container

--- header.php
+++ header.php
@@ -18,11 +18,13 @@
 			</div>
 		</div>
 
-		<div class="area-menu container">
-			<div class="row">
-				<div class="logo col-md-3">Logo</div>
-				<div class="menu-principal col-md-9 text-right">
-					<?php wp_nav_menu(array('theme_location' => 'menu_principal')); ?>
+		<div class="area-menu">
+			<div class="container">
+				<div class="row">
+					<div class="logo col-md-3">Logo</div>
+					<div class="menu-principal col-md-9 text-right">
+						<?php wp_nav_menu(array('theme_location' => 'menu_principal')); ?>
+					</div>
 				</div>
 			</div>
 		</div>

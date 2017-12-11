22b94d2b6707d31fad156220ce87134f8d853465
Raphael Giron <maingiron@gmail.com>
2017-12-11 20:33:09 -0200

    Add class to post format

--- content-image.php
+++ content-image.php
@@ -1,5 +1,5 @@
 <article>
-	<h1><?php the_title(); ?></h1>
+	<h1 class="vermelho"><?php the_title(); ?></h1>
 	<p>Categorias: <?php the_category(' '); ?></p>
 	<p><?php the_content(); ?></p>
 </article>
--- content-video.php
+++ content-video.php
@@ -1,5 +1,5 @@
 <article>
-	<h1><?php the_title(); ?></h1>
+	<h1 class="verde"><?php the_title(); ?></h1>
 	<p>Publicado em <?php echo get_the_date(); ?> por <?php the_author(); ?></p>
 	<p>Categorias: <?php the_category(' '); ?></p>
 	<p><?php the_content(); ?></p>

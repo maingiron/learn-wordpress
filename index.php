681aec29a2d03ff77db90dae8f3c8d289c2fc7ee
Raphael Giron <maingiron@gmail.com>
2017-12-11 20:10:11 -0200

    Add article

--- index.php
+++ index.php
@@ -14,12 +14,14 @@
 							// Enquanto houver algum post, chame o post...
 							while (have_posts()) : the_post();
 					?>
-						<h1><?php the_title(); ?></h1>
-						<?php the_post_thumbnail(array(175,175)); ?>
-						<p>Publicado em <?php echo get_the_date(); ?> por <?php the_author(); ?></p>
-						<p>Categorias: <?php the_category(' '); ?></p>
-						<p><?php the_tags('Tags: ', ', ') ?></p>
-						<p><?php the_content(); ?></p>
+						<article>
+							<h1><?php the_title(); ?></h1>
+							<?php the_post_thumbnail(array(175,175)); ?>
+							<p>Publicado em <?php echo get_the_date(); ?> por <?php the_author(); ?></p>
+							<p>Categorias: <?php the_category(' '); ?></p>
+							<p><?php the_tags('Tags: ', ', ') ?></p>
+							<p><?php the_content(); ?></p>
+						</article>
 					<?php 
 							endwhile;
 						else:

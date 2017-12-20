97b07591825b91857f4213d030626bc8cc15c4f4
Raphael Giron <maingiron@gmail.com>
2017-12-20 21:46:45 -0200

    Add bug img responsive

--- content-destaque.php
+++ content-destaque.php
@@ -1,6 +1,6 @@
 <article class="noticia-destaque">
 	<div class="miniatura">
-		<?php the_post_thumbnail('large', array('class' => 'img-responsive')); ?>
+		<?php the_post_thumbnail('large', array('class' => 'img-fluid')); ?>
 	</div>
 	<h1><?php the_title(); ?></h1>
 	<p>por <span><?php the_author(); ?></span> em <span><?php echo the_category(' '); ?></span> <?php the_tags('Tags: ', ', ') ?></p>
